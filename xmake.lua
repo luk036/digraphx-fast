1847f5dca57498367929d2f734386ad0825e698d
Wai-Shing Luk <luk036@gmail.com>
2026-08-13 11:15:29 +0800

    build: add fmt dependency for benchmark target
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 add_rules("mode.debug", "mode.release", "mode.coverage")
 add_requires("doctest", {alias = "doctest"})
+add_requires("fmt", {alias = "fmt"})
 add_requires("nanobench", {alias = "nanobench"})
 set_languages("c++20")
 
@@ -22,4 +23,4 @@ target("BM_neg_cycle")
     set_kind("binary")
     add_includedirs("include", {public = true})
     add_files("bench/BM_neg_cycle.cpp")
-    add_packages("nanobench")
+    add_packages("nanobench", "fmt")
